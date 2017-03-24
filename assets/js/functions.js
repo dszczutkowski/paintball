295684a5e04be910cafd34c050acdb9b99e8bd16
dszczutkowski <dszczutkowski@outlook.com>
2017-03-24 10:59:31 +0100

    naprawy błędów

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -50,7 +50,7 @@ function workLoad() {
         var
         newID = this.id,
         spinner = '<div class="spinner"> <div class="rect1"></div> <div class="rect2"></div> <div class="rect3"></div> <div class="rect4"></div> <div class="rect5"></div></div>',
-        newHtml = "./../../sprzet/" + newID +".html";
+        newHtml = "./sprzet/" + newID +".html";
     $('.sprzet-load').html(spinner).load(newHtml);
 
     });
